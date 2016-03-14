e5abca4752afe6447a90acc845a64ba3442a7a9d
Anthony Quizon <anthoq88@gmail.com>
2016-03-14 19:01:19 +1100

    initial lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to zoetrope
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/zoetrope/core_test.clj
@@ -0,0 +1,7 @@
+(ns zoetrope.core-test
+  (:require [clojure.test :refer :all]
+            [zoetrope.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
