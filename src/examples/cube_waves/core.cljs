1e1966d0c108173c9cdc6a04a66e6a7ce779cc62
Anthony Quizon <anthoq88@gmail.com>
2016-03-20 18:29:49 +1100

    circle move with mouse

--- src/examples/cube_waves/core.cljs
+++ src/examples/cube_waves/core.cljs
@@ -6,26 +6,26 @@
 
 (enable-console-print!)
 
-(defn circle []
+(defn circle [x y]
   [:circle 
    (merge dom/svg-ns 
           {:attributes {:r 100} 
-           :style      {:cx "500"
-                        :cy "500"
+           :style      {:cx x
+                        :cy y
                         :stroke-width 15
                         :fill "none"
                         :stroke "black"}})])
          
 
-(defn svg []
+(defn svg [x y]
   (dom/svg {:style {:position "absolute"
                     :width "100%" 
                     :height "100%"}}
-           (circle)))
+           (circle x y)))
   
 (defn root [inputs]
   ;{:dom (into [:div] (map (fn [x] [:h1 (str "number " x)]) (range 0 1000)))}
-  {:dom (svg)
+  {:dom (svg (get-in inputs [:mouse :position :x]) (get-in inputs [:mouse :position :y]))
   ;{:dom [:div {:style {:color "red"}} [:h1 {:style {:margin-top "20%" :font-size 70}} "hello!"]]}
   ;{:dom [:div {:style {:color "red"}} [:h1 {:style {:margin-top "20%" :font-size 70}} "hello!"] [:h2 {:style {:color "blue"}} "World!"]]
   ;{:dom [:div "hello"] 
